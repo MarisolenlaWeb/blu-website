8942daf2371ead774fad9a1ed70d0515c03eaa69
Marisol Carrillo Morales <MarisolCarrillo@MacBook-Air-de-Conecta.local>
2016-08-16 12:53:10 -0500

    Pruebas para servicios de consulta

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var request = require('superagent');
 
 // Configurar el plugin Moment
 // var moment = require('moment');
@@ -16,10 +17,19 @@ var router = express.Router();
 var title = "BLU";
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: title });
+  // res.render('index', { title: title });
+
+  request
+	  .post('http://162.243.30.11:8080/pro/Blu/api/consulta/pago')
+	  .type('form')
+	  .send({ 'fecha: ' + request.params.fecha, 'autorizacion: ' + request.params.autorizacion })
+	  .set('Accept', 'application/json')
+	  .end(function(err, res){
+	  	console.log(err);
+	  	console.log(res.body);
+	    // Calling the end function will send the request
+  	});
 });
-
-
 
 
 module.exports = router;
